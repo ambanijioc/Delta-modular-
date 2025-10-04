fe951e466569f752daacee0afddb3bf43a4a9574
ambanijioc <ambanijiocine@gmail.com>
2025-10-04 21:51:23 +0530

    Create accounts_config.py

---
+++ config/accounts_config.py
@@ -0,0 +1,16 @@
+# config/accounts_config.py
+ACCOUNTS = {
+    'account1': {
+        'bot_token': 'BOT_TOKEN_1',
+        'delta_api_key': 'DELTA_KEY_1',
+        'delta_api_secret': 'DELTA_SECRET_1',
+        'account_name': 'Main Account'
+    },
+    'account2': {
+        'bot_token': 'BOT_TOKEN_2',
+        'delta_api_key': 'DELTA_KEY_2',
+        'delta_api_secret': 'DELTA_SECRET_2',
+        'account_name': 'Secondary Account'
+    },
+    # Add more as needed
+}
